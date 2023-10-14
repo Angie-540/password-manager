3566a531a4de4ebc96764da6a46f44bedbd73ab1
Angie-540 <janiceangella21@gmail.com>
2023-10-14 19:10:07 +0300

    navbar

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'password-manager-bcda9',
+    appId: '1:418170543279:web:165c0cf96d2fa23938885a',
+    storageBucket: 'password-manager-bcda9.appspot.com',
+    apiKey: 'AIzaSyDP3jSeG572i1UYp089PBEoFDdf4pPhwmI',
+    authDomain: 'password-manager-bcda9.firebaseapp.com',
+    messagingSenderId: '418170543279',
+  },
+};
